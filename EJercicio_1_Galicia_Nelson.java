7d3b65c809890fa863f18f4adb5f79f881ac6a1d
Nelson Galicia <92438078+Pistachopower@users.noreply.github.com>
2023-12-11 21:40:03 +0100

    Examen_Uno_Java

---
+++ EJercicio_1_Galicia_Nelson.java
@@ -0,0 +1,10 @@
+
+package examen_2_java;
+
+public class EJercicio_1_Galicia_Nelson {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Ejercicio_2_Galicia_Nelson.java
@@ -0,0 +1,8 @@
+
+package examen_2_java;
+
+public class Ejercicio_2_Galicia_Nelson {
+    public static void main(String[] args) {
+        
+    }
+}
